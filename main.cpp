f12e87ef28fde5b083cb5679ed7467afd3070bc8
legleb <sedovgleb2570@gmail.com>
2025-10-25 16:34:48 +0300

    sequence uint

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@ int main()
 {
   using u_t = unsigned;
   u_t a = 0;
-  std::cin >> a;
+  size_t count = 0;
+  while (std::cin >> a) {
+    count++;
+  }
   if (std::cin.eof()) {
-    std::cout << 0;
+    std::cout << count;
     std::cout << "\n";
   } else if (std::cin.fail()) {
     std::cerr << "Error\n";
