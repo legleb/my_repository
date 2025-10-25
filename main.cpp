b2a9f881811647d6026d720740d759fb812b7bdd
legleb <sedovgleb2570@gmail.com>
2025-10-25 16:50:43 +0300

    func isPyth for uint

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
+bool isPyth(unsigned a, unsigned b, unsigned c);
+
 int main()
 {
   using u_t = unsigned;
-  u_t a = 0;
+  size_t a = 0, b = 0, c = 0;
   size_t count = 0;
   while (std::cin >> a) {
-    count += a * a;
+    count += isPyth(a, b, c);
+    c = b;
+    b = a;
   }
   if (std::cin.eof()) {
     std::cout << count;
@@ -15,4 +19,12 @@ int main()
     std::cerr << "Error\n";
     return 1;
   }
+}
+
+bool isPyth(unsigned a, unsigned b, unsigned c)
+{
+  bool p = (a * a == (b * b + c * c));
+  p = p || (b * b == (a * a + c * c));
+  p = p || (c * c == (a * a + b * b));
+  return p;
 }
